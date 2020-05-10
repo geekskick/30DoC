e426a22e0749c775fd795994aad940fc133e425c
geekskick <geeks_kick@yahoo.co.uk>
2020-05-10 17:49:07 +0100

    Done day 30

--- 30/fft/src/lib.rs
+++ 30/fft/src/lib.rs
@@ -1,37 +1,38 @@
+use rustfft::num_complex::Complex;
 use three;
 
 // https://dev.to/maniflames/audio-visualization-with-rust-4nhg
 
-pub struct UserConfig{
-    pub fpb : u32
+pub struct UserConfig {
+    pub fpb: u32,
 }
 
 pub struct ParseError;
 
-impl Default for UserConfig{
-    fn default()-> UserConfig{
-        UserConfig{
-            fpb: portaudio::FRAMES_PER_BUFFER_UNSPECIFIED
+impl Default for UserConfig {
+    fn default() -> UserConfig {
+        UserConfig {
+            fpb: portaudio::FRAMES_PER_BUFFER_UNSPECIFIED,
         }
     }
 }
 
-impl std::fmt::Display for ParseError{
+impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f,"Some error with you arguments")
+        write!(f, "Some error with you arguments")
     }
 }
 
-impl<'a> std::convert::TryFrom<&'a [String]> for UserConfig{
+impl<'a> std::convert::TryFrom<&'a [String]> for UserConfig {
     type Error = ParseError;
-    fn try_from(args: &'a [String]) -> Result<Self, Self::Error>{
-        if args.len() == 1{
+    fn try_from(args: &'a [String]) -> Result<Self, Self::Error> {
+        if args.len() == 1 {
             return Ok(UserConfig::default());
         }
 
         if args.len() == 2 {
-            if let Ok(user_fbp) = &args[1].parse::<u32>(){
-                return Ok(UserConfig{ fpb: *user_fbp });   
+            if let Ok(user_fbp) = &args[1].parse::<u32>() {
+                return Ok(UserConfig { fpb: *user_fbp });
             }
             return Err(ParseError);
         }
@@ -42,18 +43,17 @@ impl<'a> std::convert::TryFrom<&'a [String]> for UserConfig{
 // ----------------------------------------------
 
 pub struct State {
-    pub samples : Vec<f32>,
-    pub scene_meshes : Vec<three::Mesh>
+    pub samples: Vec<Complex<f32>>,
+    pub scene_meshes: Vec<three::Mesh>,
 }
 
-impl Default for State{
-    fn default() -> State{
-        State{
-            samples: Vec::new(), 
-            scene_meshes: Vec::new()
+impl Default for State {
+    fn default() -> State {
+        State {
+            samples: Vec::new(),
+            scene_meshes: Vec::new(),
         }
     }
 }
 
 // ---------------------------------
-
--- 30/fft/src/main.rs
+++ 30/fft/src/main.rs
@@ -1,11 +1,15 @@
 mod lib;
-use portaudio;
-use std::sync::mpsc;
 use crate::lib::*;
+use portaudio;
+use rustfft::num_complex::Complex;
+
+use rustfft::num_traits::Zero;
+use rustfft::FFT;
 use std::convert::TryFrom;
 use std::io::Write;
+use std::sync::mpsc;
 
-fn get_input() -> String{
+fn get_input() -> String {
     let mut guess = String::new();
 
     std::io::stdin()
@@ -16,7 +20,7 @@ fn get_input() -> String{
 // -----------------------------------
 
 fn main() {
-    let args : Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     let config = UserConfig::try_from(args.as_slice()).unwrap_or_else(|err| {
         eprintln!("Error: {}", err);
         std::process::exit(1);
@@ -27,7 +31,7 @@ fn main() {
 
     println!("Devices found:");
     let devices = pa.devices().unwrap();
-    for device in devices{
+    for device in devices {
         let (idx, info) = device.unwrap();
         println!("{:?} = {:?}", idx, info);
     }
@@ -36,15 +40,17 @@ fn main() {
 
     print!("Which device do you want to use? Leave blank for default\n>\t");
     std::io::stdout().flush().expect("Unable to flush stdout");
-    let choice : u32 = get_input().trim().parse().unwrap_or(default);
+    let choice: u32 = get_input().trim().parse().unwrap_or(default);
 
     let mic_index = portaudio::DeviceIndex(choice);
     println!("Using device: {:?}", mic_index);
 
-    let mic = pa.device_info(mic_index).expect("Unable to get mic instance");
+    let mic = pa
+        .device_info(mic_index)
+        .expect("Unable to get mic instance");
     println!("{:?}", mic);
 
-    if mic.max_input_channels == 0{
+    if mic.max_input_channels == 0 {
         eprintln!("Need to have at least 1 input channel for this application.");
         std::process::exit(1);
     }
@@ -52,7 +58,8 @@ fn main() {
     let num_channels = 1;
     let interleave = true; // This matters!
     let latency = mic.default_low_input_latency;
-    let input_params = portaudio::StreamParameters::<f32>::new(mic_index, num_channels, interleave, latency);
+    let input_params =
+        portaudio::StreamParameters::<f32>::new(mic_index, num_channels, interleave, latency);
     println!("{:?}", input_params);
 
     let fs = mic.default_sample_rate;
@@ -62,14 +69,14 @@ fn main() {
     println!("{:?}", input_settings);
 
     let (sender, consumer) = mpsc::channel();
-    let cb = move |portaudio::InputStreamCallbackArgs{ buffer, ..} | {
-        match sender.send(buffer){
-            Ok(_) => portaudio::Continue,
-            Err(_) => portaudio::Complete
-        }
+    let cb = move |portaudio::InputStreamCallbackArgs { buffer, .. }| match sender.send(buffer) {
+        Ok(_) => portaudio::Continue,
+        Err(_) => portaudio::Complete,
     };
 
-    let mut stream = pa.open_non_blocking_stream(input_settings, cb).expect("Unable to create stream");
+    let mut stream = pa
+        .open_non_blocking_stream(input_settings, cb)
+        .expect("Unable to create stream");
     let mut win = get_window();
     let mut state = State::default();
 
@@ -77,13 +84,12 @@ fn main() {
 
     stream.start().expect("Unable to start streaming");
 
-    while win.update() && !win.input.hit(three::KEY_ESCAPE){
-
+    while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         update_lines(&mut win, &mut state);
         win.render(&camera);
 
         remove_lines(&mut win, &mut state);
-        while let Ok(buffer) = consumer.try_recv(){
+        while let Ok(buffer) = consumer.try_recv() {
             update_samples(&buffer, &mut state);
         }
     }
@@ -99,41 +105,46 @@ fn get_window() -> three::window::Window {
     win
 }
 
-fn update_lines(win: &mut three::window::Window, state: &mut State){
+fn update_lines(win: &mut three::window::Window, state: &mut State) {
+    for (index, y_position) in state.samples.iter().enumerate() {
+        let i = index as f32;
+        let num_samples = state.samples.len() as f32;
+        let scale = 3.0;
+        let x_pos = (i / (num_samples / scale)) - (0.5 * scale);
 
-    for(index, y_position) in state.samples.iter().enumerate(){
-         let i = index as f32;
-         let num_samples = state.samples.len() as f32;
-         let scale = 3.0;
-         let x_pos = (i/(num_samples/scale)) - (0.5 * scale);
+        let geometry = three::Geometry::with_vertices(vec![
+            [x_pos, y_position.norm(), 0.0].into(),
+            [x_pos, -y_position.norm(), 0.0].into(),
+        ]);
 
-         let geometry = three::Geometry::with_vertices(vec![
-            [x_pos, y_position.clone(), 0.0].into(), 
-            [x_pos, -y_position.clone(), 0.0].into()
-         ]);
+        let color = 0x00ff00;
+        let material = three::material::Line { color: color };
 
-         let r = *y_position * 1024.0;
-         let r_abs = std::cmp::min(r.abs() as u32, 255);
-         let g = 0xff - r_abs;
-         let color = (r_abs << 16) | (g << 8) | r_abs;
-         let material = three::material::Line{
-             color: color,
-         };
+        let mesh = win.factory.mesh(geometry, material);
+        win.scene.add(&mesh);
 
-         let mesh = win.factory.mesh(geometry, material);
+        state.scene_meshes.push(mesh);
 
-         win.scene.add(&mesh);
-         state.scene_meshes.push(mesh);
     }
 }
 
-fn remove_lines(win : &mut three::window::Window, state:&mut State){
-    for mesh in &state.scene_meshes{
+fn remove_lines(win: &mut three::window::Window, state: &mut State) {
+    for mesh in &state.scene_meshes {
         win.scene.remove(&mesh);
     }
     state.scene_meshes.clear();
 }
 
-fn update_samples(samples:&[f32], state : &mut State){
-    state.samples = samples.to_vec();
+fn update_samples(samples: &[f32], state: &mut State) {
+    let mut input: Vec<Complex<f32>> = samples
+        .iter()
+        .map(|sample| Complex::new(*sample, 0.0))
+        .collect();
+
+    let mut output: Vec<Complex<f32>> = vec![Complex::zero(); input.len()];
+
+    let fft = rustfft::algorithm::Radix4::new(input.len(), false);
+    fft.process(&mut input, &mut output);
+
+    state.samples = output;
 }
